29d2830a6f3b1fe89da771ecdd80367b71925960
Martti11 <103411896+Martti11@users.noreply.github.com>
2022-05-04 12:05:05 +0300

    loeng08

---
+++ loeng08/harjutus.js
@@ -0,0 +1,21 @@
+let kasutajaNimi = prompt("Sisesta nimi")
+
+let tervitusElement = document.getElementById("tervitus")
+
+if (kasutajaNimi === "Kalle" || kasutajaNimi === "Malle") {
+    tervitusElement.innerHTML = "<h2>Tere, kallis sõber " + kasutajaNimi +"</h2>"
+    for (let index = 1; index <=10; index += 1) {           
+    tervitusElement.innerHTML += `<p>tere-tere ${index}. korda</p>`
+    }
+}   else if (!kasutajaNimi) {
+        tervitusElement.innerHTML =`<p class="punane">Jätsid nime sisestamata</p>`
+} 
+
+else {
+    tervitusElement.innerHTML = `<p>Tere, ${kasutajaNimi} </p>`
+    tervitusElement.innerHTML += `<h2>Tähed sinu nimes:</h2>`
+    for (let index = 0; index < kasutajaNimi.length; index++ ) {
+        tervitusElement.innerHTML += `<div>Täht nr ${index}:${kasutajaNimi[index]}</div>`
+    }
+
+}
